98aa7e9de75ac6d286ddd77a6d5b944a4f84efe4
Student <student@lab-12.local>
2013-01-30 21:46:14 -0500

    First save

---
+++ people/shelby/1/this-week.js
@@ -0,0 +1,31 @@
+// Exercise 1.1.1 - Fix me!
+alert("Hello, World!");
+
+// Exercise 1.1.2 - Alert anything else
+alert("Shelby is learning Javascript!");
+
+
+// Exercise 1.2.1 - Create a new variable that is the sum of a and b. Alert it to the user.
+var a = 10;
+var b = 56;
+var c = a+b;
+alert(c);
+
+
+// Exercise 1.2.2 - Try joining these strings together with a +. Alert it to the user.
+var message = "Hello";
+var message2 = ", World!";
+var message3 = message+message2;
+alert(message3);
+
+
+// Exercise 1.2.3 - Prompt allows you to get input from the user. Given the username, alert a personalized message.
+var username = prompt("What is your name?", "");
+alert("Hello, "+username+"!");
+
+
+
+// Exercise 1.2.4 - Ask the user for their name. Then ask for their age. Alert a message back combining both values into a sentence.
+var yourname = prompt("What is your name?", "");
+var age = prompt("What is your age?", "");
+alert("welcome, "+yourname"You are "+age+" years old.");
