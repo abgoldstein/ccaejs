9f015011d41b79fe4c7e0e0aa5e5539715e06e29
Student <student@Change-Me-3.local>
2013-02-06 21:30:04 -0500

    Correcting a typo in adam's week 2 code

---
+++ people/David/2/this-week.js
@@ -0,0 +1,44 @@
+// Exercise 1.review - Ask the user for their name. Welcome them with a personalized message.
+alert("What is your name?");
+// 2.1 - Conditionals
+// Exercise 2.1.1 - Ask the user for 2 numbers. Tell them if the first or second number was bigger.
+
+alert("hahahahahah");
+
+// Exercise 2.1.2 - Ask the user for a number. Tell the user whether it is odd or even.
+
+
+// Exercise 2.1.3 - Ask the user what month they were born in. Tell them what season that month is in.
+
+
+
+// 2.2 - Loops
+// Exercise 2.2.1 - Use a loop to count from 10 to 1. Alert each value as you count.
+
+
+// Exercise 2.2.2 - Use a loop to count from 10 to 20. Add each number to a total sum and alert that value at the end.
+
+
+// Exercise 2.2.3
+// Generate a random number (I haven't told you how to do this. Try Googling for it)
+// Ask the user to try to guess the number (Give them the possible range)
+// Tell the user if they got the number or if they were too high or too low.
+// Let the user continue guessing until they get to the right answer.
+
+
+
+// 2.3 - Arrays
+// Create an array of usernames. They can be anything.
+// Ask the user for their name. Check to see if that name is on the list.
+// If the user is on the list, welcome them in. Otherwise, tell them to leave!
+// Bonus: Now that this is working, try Googling for an easier way to do this.
+
+
+// Exercise 2.3.2
+// Generate 10 random numbers and show them all to the user.
+// Ask the user for the index of the biggest number. Tell them whether or not they were right.
+
+
+// Exercise 2.3.3 - Modify your code from exercise 2.2.3 to allow the user to replay.
+// Add a scoreboard array to keep track of how many guesses it took them to win.
+// Show the user the scoreboard each time and congratulate them if they beat their best.
