32929f7a7053d5c6e7ad7ef00de40b8e4c68b6e6
Student <student@lab-5.local>
2013-02-13 21:38:26 -0500

    arrays

--- people/cassandra_R/2/this-week.js
+++ people/cassandra_R/2/this-week.js
@@ -1,10 +1,10 @@
 // Exercise 1.review - Ask the user for their name. Welcome them with a personalized message.
-	var username = prompt(" what's your name?");
+	/*var username = prompt(" what's your name?");
 	alert("Welcome "+username+"!");
-
+	*/
 // 2.1 - Conditionals
 // Exercise 2.1.1 - Ask the user for 2 numbers. Tell them if the first or second number was bigger.
-	var num1 = prompt("What's the first number?");
+	/*var num1 = prompt("What's the first number?");
 	var num2 = prompt("What's the second number?");
 
 	if (num1 > num2) {
@@ -18,8 +18,10 @@
 	else {
 		alert("I'm confused");
 	}
+	*/
+
 // Exercise 2.1.2 - Ask the user for a number. Tell the user whether it is odd or even.
-	var numb = prompt("Give me an even or odd number.");
+	/*var numb = prompt("Give me an even or odd number.");
 
 	if (numb % 2 == 0) {
 		alert(numb+" is even!")
@@ -27,6 +29,7 @@
 	else {
 		alert(numb+" is NOT an even number!")
 	};
+	*/
 
 // Exercise 2.1.3 - Ask the user what month they were born in. Tell them what season that month is in.
 
@@ -49,10 +52,21 @@
 
 // 2.3 - Arrays
 // Create an array of usernames. They can be anything.
+var arrayOfUnames = ["ADAM", "CASSANDRA", "DAVE", "DOUG", "EMILY", "GRETCHEN", "RADA", "SARAH", "SHELBY"];
 // Ask the user for their name. Check to see if that name is on the list.
+var yourName = prompt("Halt! Who goes there?");
+
+var yourName = yourName.toUpperCase();
+
 // If the user is on the list, welcome them in. Otherwise, tell them to leave!
 // Bonus: Now that this is working, try Googling for an easier way to do this.
+if (arrayOfUnames.indexOf(yourName) == -1){
+	alert("No-go good buddy!");
+}
 
+else {
+	alert("You're in!");
+}
 
 // Exercise 2.3.2
 // Generate 10 random numbers and show them all to the user.
