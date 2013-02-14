966936d3dce851e3b580a5e3eabcee9964ac7849
Adam B. Goldstein <abgoldstein@gmail.com>
2013-02-13 19:25:59 -0500

    Adding week 3 materials.

--- weeks/3/this-week.js
+++ weeks/3/this-week.js
@@ -1,44 +1,22 @@
-// Exercise 1.review - Ask the user for their name. Welcome them with a personalized message.
+// Exercise 3.1 - Functions
+// Exercise 3.1.1 - Write a sum function. It should take an array of numbers and return the sum of them.
 
 
-// 2.1 - Conditionals
-// Exercise 2.1.1 - Ask the user for 2 numbers. Tell them if the first or second number was bigger.
+// Exercise 3.1.2 - What is the default return value of a function? Write some code to demonstrate your answer.
 
 
-// Exercise 2.1.2 - Ask the user for a number. Tell the user whether it is odd or even.
+// Exercise 3.1.3 - Write the Caesar Cipher encrypt function.
+// A Caesar cipher is a very basic style of encryption where all letters are shifted by a number of letters.
+// For example, if the phrase is "Dog" and the cipher is 2, the encrypted message becomes "Fqi" because D->E->F, o->p->q, and g->h->i.
+// Ask the user for a sentence and a cipher, then use your encrypting function and display your results.
+//
+// More details can be found here - http://en.wikipedia.org/wiki/Caesar_cipher
+// I have excluded a bunch of information here. You'll probably need an ASCII chart, a way to get substrings, and to count characters.
+// Try using the internet to fill in your missing toolset. I can help you if you get stuck.
 
 
-// Exercise 2.1.3 - Ask the user what month they were born in. Tell them what season that month is in.
+// Exercises 3.1.4 - Write the Caesar Cipher decrypt function.
+// Write a function to take an encrypted message and decrypt it with a given cipher. Show the user the decrypted message.
 
 
-
-// 2.2 - Loops
-// Exercise 2.2.1 - Use a loop to count from 10 to 1. Alert each value as you count.
-
-
-// Exercise 2.2.2 - Use a loop to count from 10 to 20. Add each number to a total sum and alert that value at the end.
-
-
-// Exercise 2.2.3
-// Generate a random number (I haven't told you how to do this. Try Googling for it)
-// Ask the user to try to guess the number (Give them the possible range)
-// Tell the user if they got the number or if they were too high or too low.
-// Let the user continue guessing until they get to the right answer.
-
-
-
-// 2.3 - Arrays
-// Create an array of usernames. They can be anything.
-// Ask the user for their name. Check to see if that name is on the list.
-// If the user is on the list, welcome them in. Otherwise, tell them to leave!
-// Bonus: Now that this is working, try Googling for an easier way to do this.
-
-
-// Exercise 2.3.2
-// Generate 10 random numbers and show them all to the user.
-// Ask the user for the index of the biggest number. Tell them whether or not they were right.
-
-
-// Exercise 2.3.3 - Modify your code from exercise 2.2.3 to allow the user to replay.
-// Add a scoreboard array to keep track of how many guesses it took them to win.
-// Show the user the scoreboard each time and congratulate them if they beat their best.
+// Exercise 3.1.5 - Test it! Try encrypting a string and then sending it to a neighbor for decryption. How do you do?
