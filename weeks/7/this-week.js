74731b5fbab720f52507690f85d10f54c92b7360
Adam B. Goldstein <abgoldstein@gmail.com>
2013-03-13 16:01:01 -0400

    Adding a little more week 6 content.

--- weeks/7/this-week.js
+++ weeks/7/this-week.js
@@ -1,7 +1,20 @@
 document.write("<pre>");
 
-// Exercises 7.1
-// Exercise 7.1.1 - 
+// Exercises 7 - Data Structures
+// Exercises 7.1 - Multidimensional Arrays
 //
+// Exercise 7.1.1 - Below is an arbitrary two dimensional array. Print out the second value of the third row.
+
+
+// Exercise 7.1.2 - Below is a list of players' scores for pinball. Who has played the most times?
+
+
+// Exercises 7.1.3 - Using the same pinball score list, check to see what the highest score was.
+
+
+
+// Exercises 7.2 - Objects
+// Exercise 7.2.1
+
 
 document.write("</pre");
